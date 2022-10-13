c9de29db2267f8e4b4768d80fbf65626cc6b32c7
TheRoam <63456390+TheRoam@users.noreply.github.com>
2022-10-13 20:03:07 +0200

    v1.1
    optimised coding and performance, added warmup option

--- XIAO_ESP32-C3_AnalogTouch.ino
+++ XIAO_ESP32-C3_AnalogTouch.ino
@@ -1,5 +1,5 @@
 //-------------------------------------------------------------------------------//
-//------------------------ XIAO ESP32-C3 AnalogTouch 1.0 ------------------------//
+//------------------------ XIAO ESP32-C3 AnalogTouch 1.1 ------------------------//
 //                                                                               //
 // Based on AnalogRead example script for Esp32 library by Espressif             //
 // Usage & principles:                                                           //
@@ -19,28 +19,41 @@
 //                                                                               //
 //-------------------------------------------------------------------------------//
 
-// OUTPUT verbosity
+
+    // OUTPUT verbosity
 // 1: extended --> Prints ADC array, mean value, touch count and touch message with touch count
 // 0: minimal  --> Prints touch message with touch count
-int out=1;
+#define OUT 0
 
 // comparison variables
-int MV=20;        //values for mean array
-int mean[20]={};  //change 
+#define VALUES 20       //values for mean array
+#define TOUCHPIN 3      //touch pin
+#define VALINTERVAL 100 //evaluation interval
+#define REFV 80         //reference voltage drop in mV
+#define WARMUP 0        //delay time after reboot before evaluation starts
+
+// Calibration:
+// 1. Set a high REFV, for example 1000
+// 2. Run the extended script with OUT 1
+// 3. Open Serial Monitor and have a look at the average values for a while (e.g.: 230)
+// 4. Now touch the jumper wire and see what the values are now (e.g.: 170)
+// 5. Make the difference between the average at 3. and the average at 4 (230-170=60).
+// 6. Set your REFV to this value (60) and play around it.
+//    -- For more sensitivity, set it a bit lower (50)
+//    -- If there is detection with no touch, increase it (80)
+//    -- Because of CPU load, REFV value will vary if OUT 1 or OUT 0
+//    -- Touch could triggers itself for a few seconds during start, as analog readings are higher.
+
+
+int out=OUT;
+int wmp=WARMUP;
+int mean[VALUES]={};  //change 
 int avg;
 int fin;
-int refV=200;     //voltage drop that triggers detection (mV)
-
-// touch pin (Define any ADC pin on your board)
-int TP=3;
+int TP=TOUCHPIN;
 
 // touch counter
 int touch=0;
-
-// pin reading time (higher value, lower CPU load, but lower precison)
-// 100ms is usually OK
-
-int d=100;
 
 // Touch the pin for at least this time for touch to be recognised,
 // i.e.: if d=1000ms (1 second) touch the pin for at least 1 second;
@@ -49,105 +62,111 @@ int d=100;
 // This can be used as "hold" recognition.
 
 void setup() {
-  
   // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
-  
-  //set the resolution to 12 bits (0-4096)
-  analogReadResolution(12);
+}
 
+void loop() {
+  //wait for board to normalise
+  if(wmp==1){
+    Serial.println("Warming up....");
+    delay(WARMUP*1000);
+    Serial.println("Start readings!");
+    wmp=0;
+  }
+  setTouch(TOUCHPIN);
+  //run check
+  if(out==1){
+    extended(TP);
+  }else if(out==0){
+    minimal(TP);
+  }
+}
+
+void setTouch(int tp){
   //fill up the array
-  for(int a=0;a<MV;a++){
-    mean[a]=analogRead(TP);
+  for(int a=0;a<VALUES;a++){
+    mean[a]=analogRead(tp);
+    
     delay(10);
   }
 }
 
-void loop() {
-
-  if(out==1){
-    extended();
-  }else if(out==0){
-    minimal();
-  }
-  
-}
-
-void extended(){
-  //start reading values
-  for(int i=0;i<MV;i++){
-    // read value
-    mean[i]=analogRead(TP);
+int extended(int tp){
+  for(int i=0;i<VALUES;i++){
+    // send value to array
+    mean[i]=analogRead(tp);
     //start printing array in monitor
     Serial.println("Array: [");
 
     //read final value
-    fin=analogRead(TP);
+    fin=analogRead(tp);
 
+    Serial.print("Last: ");
+    Serial.println(fin);
     //get average of array
-    for(int j=0;j<MV;j++){
+    //start printing array
+    Serial.println("Array: [");
+    for(int j=0;j<VALUES;j++){
       avg+=mean[j];
+      //print value
       Serial.print(mean[j]);
-      Serial.print(", ");
+      Serial.print(", "); 
     }
+    //close array
     Serial.println("]");
     
-    avg=avg/MV;
+    avg=avg/VALUES;
     Serial.print("Average: ");
     Serial.println(avg);
-
+    
     //compare average with last value
-    if((avg-fin)>refV){
+    if((avg-fin)>REFV){
       Serial.println("----------  T   O    U    C    H  ----------");
       //increase counter
       touch++;
-      Serial.print("Touch count: ");
-      Serial.println(touch);
       //wait 1 second after touch detected
       delay(1000);
     }
     //restart counter
-    if(i==(MV-1)){
-      i=-1;
-    }
+    avg=0;
     //print touch count
     Serial.print("T: ");
     Serial.println(touch);
-    
-    delay(d);
+    delay(VALINTERVAL);
   }
+  return touch;
 }
 
-void minimal(){
+int minimal(int tp){
   //start reading values
-  for(int i=0;i<MV;i++){
+  for(int i=0;i<VALUES;i++){
     // read value
-    mean[i]=analogRead(TP);
+    mean[i]=analogRead(tp);
 
     //read final value
-    fin=analogRead(TP);
-
+    fin=analogRead(tp);
     //get average of array
-    for(int j=0;j<MV;j++){
+    for(int j=0;j<VALUES;j++){
       avg+=mean[j];
     }
     
-    avg=avg/MV;
-
+    avg=avg/VALUES;
+    
     //compare average with last value
-    if((avg-fin)>refV){
+    if((avg-fin)>REFV){
       Serial.println("----------  T   O    U    C    H  ----------");
       //increase counter
       touch++;
-      Serial.print("Touch count: ");
+      //print touch count
+      Serial.print("T: ");
       Serial.println(touch);
       //wait 1 second after touch detected
       delay(1000);
     }
     //restart counter
-    if(i==(MV-1)){
-      i=-1;
-    }
-    delay(d);
+    avg=0;
+    delay(VALINTERVAL);
   }
+  return touch;
 }
